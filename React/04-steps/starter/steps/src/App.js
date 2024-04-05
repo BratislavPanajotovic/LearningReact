d72eb59cae6fc2e1585553df6c0b216e7353d492
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-04-05 21:20:47 +0200

    fnished 1/4 of course !

--- React/04-steps/starter/steps/src/App.js
+++ React/04-steps/starter/steps/src/App.js
@@ -6,7 +6,15 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
-function App() {
+export default function App() {
+  return (
+    <div>
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isActive, setIsActive] = useState(true);
   const handlePrevious = () => {
@@ -33,18 +41,17 @@ function App() {
               Step {step}:{messages[step - 1]}{" "}
             </p>
             <div className="buttons">
-              <button
-                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              <Button
+                bgColor="#7950f2"
+                textColor="#fff"
                 onClick={handlePrevious}
               >
-                Previous
-              </button>
-              <button
-                style={{ backgroundColor: "#7950f2", color: "#fff" }}
-                onClick={handleNext}
-              >
-                Next
-              </button>
+                <span>👈</span> Previous
+              </Button>
+
+              <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+                <span>👉</span> Next
+              </Button>
             </div>
           </>
         </div>
@@ -53,4 +60,13 @@ function App() {
   );
 }
 
-export default App;
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
