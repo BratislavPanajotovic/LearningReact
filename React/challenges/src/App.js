feb1fce1411c6ee5e8983aee731e2c6fb0722df2
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-04-16 20:50:10 +0200

    nice challenge started

--- React/challenges/src/App.js
+++ React/challenges/src/App.js
@@ -174,71 +174,131 @@ import { useState } from "react";
 // !!! THIRD CHALLENGE FINISHED
 
 // *** FOURTH CHALLENGE - TIP CALCULATOR
+// function App() {
+//   return (
+//     <div className="App">
+//       <BillSum />
+//       <Service />
+//       <FriendService />
+//       <Stats />
+//       <Reset />
+//       {/* <DateCounter /> */}
+//       {/* <FlashCards /> */}
+//     </div>
+//   );
+// }
+
+// function BillSum() {
+//   const [bill, setBill] = useState(0);
+//   function handleInput() {
+//     setBill((e) => Number(e.target.value));
+//   }
+//   return (
+//     <div>
+//       <h3>How much was the bill ?</h3>
+//       <input type="number" value={bill} onChange={handleInput} />
+//     </div>
+//   );
+// }
+// function Service() {
+//   return (
+//     <div>
+//       <h3>How did you like the service ?</h3>
+//       <select type="number" name="" id="">
+//         <option value="dissatisfied">Dissatisfied (0%)</option>
+//         <option value="ok">It was ok (5%)</option>
+//         <option value="good">It was good (10%)</option>
+//         <option value="amazing">Absolutely amazing (20%)</option>
+//       </select>
+//     </div>
+//   );
+// }
+// function FriendService() {
+//   return (
+//     <div>
+//       <h3>How did your friend like the service?</h3>
+//       <select type="number" name="" id="">
+//         <option value="dissatisfied">Dissatisfied (0%)</option>
+//         <option value="ok">It was ok (5%)</option>
+//         <option value="good">It was good (10%)</option>
+//         <option value="amazing">Absolutely amazing (20%)</option>
+//       </select>
+//     </div>
+//   );
+// }
+// function Stats() {
+//   return (
+//     <div>
+//       <h2>You pay X ( X + X tip )</h2>
+//     </div>
+//   );
+// }
+
+// function Reset() {
+//   return (
+//     <div>
+//       <button type="button">RESET</button>
+//     </div>
+//   );
+// }
+//! FOURTH CHALLENGE FINISHED
+
+//* FIFTH CHALLENGE :
 function App() {
   return (
-    <div className="App">
-      <BillSum />
-      <Service />
-      <FriendService />
-      <Stats />
-      <Reset />
-      {/* <DateCounter /> */}
-      {/* <FlashCards /> */}
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
     </div>
   );
 }
 
-function BillSum() {
-  const [bill, setBill] = useState(0);
-  function handleInput() {
-    setBill((e) => Number(e.target.value));
-  }
+function TextExpander({
+  children,
+  collapsedNumWords = 0,
+  expandButtonText = "Show more",
+  collapseButtonText = "Hide text",
+  buttonColor = "#3a7bd5",
+  expanded = false,
+  className = "box",
+}) {
+  const [collNumWords, setCollNumWords] = useState(collapsedNumWords);
+  const boxStyle = {
+    className: `${className}`,
+  };
   return (
     <div>
-      <h3>How much was the bill ?</h3>
-      <input type="number" value={bill} onChange={handleInput} />
-    </div>
-  );
-}
-function Service() {
-  return (
-    <div>
-      <h3>How did you like the service ?</h3>
-      <select type="number" name="" id="">
-        <option value="dissatisfied">Dissatisfied (0%)</option>
-        <option value="ok">It was ok (5%)</option>
-        <option value="good">It was good (10%)</option>
-        <option value="amazing">Absolutely amazing (20%)</option>
-      </select>
-    </div>
-  );
-}
-function FriendService() {
-  return (
-    <div>
-      <h3>How did your friend like the service?</h3>
-      <select type="number" name="" id="">
-        <option value="dissatisfied">Dissatisfied (0%)</option>
-        <option value="ok">It was ok (5%)</option>
-        <option value="good">It was good (10%)</option>
-        <option value="amazing">Absolutely amazing (20%)</option>
-      </select>
-    </div>
-  );
-}
-function Stats() {
-  return (
-    <div>
-      <h2>You pay X ( X + X tip )</h2>
+      <p style={boxStyle}>{children} </p>
+      <button onClick={() => setCollNumWords(collNumWords + 1)}>
+        {expandButtonText}
+      </button>
     </div>
   );
 }
 
-function Reset() {
-  return (
-    <div>
-      <button type="button">RESET</button>
-    </div>
-  );
-}
 export default App;
