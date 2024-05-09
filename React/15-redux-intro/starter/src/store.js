e5ecd4fab28cca4d358c180cac1b04696eb9b4bb
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-05-09 15:15:24 +0200

    working with action creators

--- React/15-redux-intro/starter/src/store.js
+++ React/15-redux-intro/starter/src/store.js
@@ -1,5 +1,7 @@
+import { createStore } from "redux";
+
 const initialState = {
-  balance,
+  balance: 0,
   loan: 0,
   loanPurpose: "",
 };
@@ -11,14 +13,19 @@ function reducer(state = initialState, action) {
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
-        if (state.loan > 0) return state;
-        return {...state, loan: action.payload};
-        case 'account/payloan':
-            return {...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan}
+      if (state.loan > 0) return state;
+      return { ...state, loan: action.payload };
+    case "account/payloan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
   }
-
-
-
-  default:
-    return state;
 }
+
+const store = createStore(reducer);
+store.dispatch({ type: "account/deposit", payload: 500 });
