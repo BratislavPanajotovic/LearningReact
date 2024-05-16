46454b7225f2ba0e2e0d59264d6dfa0cefdf815d
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-05-17 00:53:18 +0200

    dispatching actions from react to redux

--- React/15-redux-intro/starter/src/features/accounts/AccountOperations.js
+++ React/15-redux-intro/starter/src/features/accounts/AccountOperations.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { deposit } from "./accountSlice";
+import { deposit, withdraw, requestLoan, payLoan } from "./accountSlice";
 
 function AccountOperations() {
   const [depositAmount, setDepositAmount] = useState("");
@@ -10,7 +10,11 @@ function AccountOperations() {
   const [currency, setCurrency] = useState("USD");
 
   const dispatch = useDispatch();
-  const account = useSelector((state) => state.account);
+  const {
+    loan: currentLoan,
+    loanPurpose: currentLoanPurpose,
+    balance,
+  } = useSelector((state) => state.account);
 
   function handleDeposit() {
     if (!depositAmount) return;
@@ -18,11 +22,24 @@ function AccountOperations() {
     setDepositAmount("");
   }
 
-  function handleWithdrawal() {}
+  function handleWithdrawal() {
+    if (!withdrawalAmount) return;
+    dispatch(withdraw(withdrawalAmount));
+    setWithdrawalAmount("");
+  }
 
-  function handleRequestLoan() {}
+  function handleRequestLoan() {
+    if (!loanAmount || !loanPurpose) return;
+    dispatch(requestLoan({ loanAmount, loanPurpose }));
+    setLoanAmount("");
+    setLoanPurpose("");
+  }
 
-  function handlePayLoan() {}
+  function handlePayLoan() {
+    if (!loanAmount) return;
+    dispatch(payLoan(""));
+    setLoanAmount("");
+  }
 
   return (
     <div>
@@ -76,7 +93,9 @@ function AccountOperations() {
         </div>
 
         <div>
-          <span>Pay back $X</span>
+          <span>
+            Pay back ${currentLoan} ({currentLoanPurpose})
+          </span>
           <button onClick={handlePayLoan}>Pay loan</button>
         </div>
       </div>
